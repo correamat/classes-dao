e885714ecfa675827a3dfd5d0f4b73f7e2d9f4f4
Matheus Correa <mvcmatheus12@gmail.com>
2018-06-13 17:17:03 -0300

    algumas funcoes implementadas

--- Class/Usuario.php
+++ Class/Usuario.php
@@ -59,6 +59,48 @@
 
 		}
 
+		public static function getList(){
+
+			$sql = new Sql();
+
+			return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+
+		}
+
+		public static function search($login){
+
+			$sql = new Sql();
+
+			return  $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :search ORDER BY deslogin",array(
+				':search'=>"%".$login."%"	
+			));
+		}
+
+		//OBTENDO OS DADOS DE UM USUARIO AUTENTICADO
+		public function login($login,$password){
+
+			$sql = new Sql();
+
+			$result = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :login AND dessenha = :senha",array(
+
+				":login"=>$login,
+				":senha"=>$password));
+
+			if(count($result)>0){
+
+				$row = $result[0];
+
+				$this->setIdusuario($row['idusuario']);
+				$this->setLogin($row['deslogin']);
+				$this->setSenha($row['dessenha']);
+				$this->setDataCadastro(new DateTime($row['dtcadastro']));
+			}else{
+
+				throw new Exception("Login e/ou senha inválidos");
+				
+			}
+
+		}
 		public function __toString(){
 
 			return json_encode(array("idusuario"=>self::getIdusuario(),
@@ -68,6 +110,8 @@
 
 			));
 		}
+
+
 
 	}
 
--- index.php
+++ index.php
@@ -1,14 +1,24 @@
 <?php 
 	require_once("Config.php");
-
-	$user = new Usuario();
-
-	$user->loadById(1);
-
-	echo $user;
+	//CARREGA UM USUARIO DO ID PASSADO POR PARAMETRO
+	//$user = new Usuario();
+	//$user->loadById(1);
+	//echo $user;
 	//$sql = new Sql();
-
 	//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 	//echo json_encode($usuarios);
+
+	//CARREGA UMA LISTA DE USUARIOS
+	//$lista = Usuario::getList();
+	//echo json_encode($lista);
+
+	// CARREGA UMA LISTA DE USUARIOS BUSCANOD PELO LOGIN
+	//$search = Usuario::search("us");
+	//echo json_encode($search);
+
+	//CARREGA UM USUARIO USANDO O LOGIN E UMA SENHA
+
+	$usuario = new Usuario();
+	$usuario->login("user","12345");
+	echo $usuario;
  ?>
